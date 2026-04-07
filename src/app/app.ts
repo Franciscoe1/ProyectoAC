ede4453aeb550e05788215873258b5ae058e0aae
Franciscoe1 <franciscoxav522@gmail.com>
2026-04-06 22:31:11 -0500

    Primer commit Front y Back funcionando, DB funcionando

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,38 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+
+  productos: any[] = [];
+
+  constructor(
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef // 👈 IMPORTANTE
+  ) {}
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:8000/api/productos/')
+      .subscribe({
+        next: (data) => {
+          console.log("DATA:", data);
+
+          this.productos = data.productos;
+
+          console.log("PRODUCTOS:", this.productos);
+
+          this.cdr.detectChanges(); // 🔥 SOLUCIÓN
+        },
+        error: (err) => {
+          console.error("ERROR:", err);
+        }
+      });
+  }
 }

e7ac238725b6a4282db9837f24550999ee115c48
Franciscoe1 <franciscoxav522@gmail.com>
2026-04-09 20:18:22 -0500

    Inicio Front terminado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -13,9 +15,20 @@ export class App implements OnInit {
 
   productos: any[] = [];
 
+  // 🔥 SLIDER
+  imagenes = [
+    'banner1.png',
+    'banner2.png',
+    'banner3.png'
+  ];
+  indice = 0;
+
+  // 🔥 EMAIL FORM
+  email: string = "";
+
   constructor(
     private http: HttpClient,
-    private cdr: ChangeDetectorRef // 👈 IMPORTANTE
+    private cdr: ChangeDetectorRef
   ) {}
 
   ngOnInit() {
@@ -28,11 +41,54 @@ export class App implements OnInit {
 
           console.log("PRODUCTOS:", this.productos);
 
-          this.cdr.detectChanges(); // 🔥 SOLUCIÓN
+          this.cdr.detectChanges();
         },
         error: (err) => {
           console.error("ERROR:", err);
         }
       });
+
+    // 🔥 AUTO SLIDER
+    setInterval(() => {
+      this.siguiente();
+    }, 5000);
+  }
+
+  // 🔥 SLIDER
+  siguiente() {
+    this.indice = (this.indice + 1) % this.imagenes.length;
+  }
+
+  anterior() {
+    this.indice =
+      (this.indice - 1 + this.imagenes.length) % this.imagenes.length;
+  }
+
+  // 🔥 BOTÓN PEDIDOS
+  irAPedidos() {
+    console.log("Ir a pedidos corporativos");
+  }
+
+  // 🔥 FORMULARIO DJANGO
+  registrarEmail(event: Event) {
+    event.preventDefault();
+
+    if (!this.email) {
+      alert("Ingresa un email válido");
+      return;
+    }
+
+    this.http.post('http://localhost:8000/api/suscribirse/', {
+      email: this.email
+    }).subscribe({
+      next: () => {
+        alert("¡Registrado correctamente! 🎉");
+        this.email = "";
+      },
+      error: (err) => {
+        console.error(err);
+        alert("Error al registrar el email");
+      }
+    });
   }
 }
